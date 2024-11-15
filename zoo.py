ba9fd24d0a64f730a24d3925c93feea7545a26b2
Prapat Jindarat <93991753+DoublePJ@users.noreply.github.com>
2024-11-15 05:55:20 +0000

    Zoo-ticket test case

--- zoo.py
+++ zoo.py
@@ -1,10 +1,15 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12:   #correctly
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: #correctly
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: #correctly
             return 150
-        elif age >= 60:
+        elif age > 60: #correctly
             return 100
+        
+
+if __name__ == "__main__":
+    zoo = Zoo()
+    print(zoo.get_ticket_price(-5))
